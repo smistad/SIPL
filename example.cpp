d2c9fa94bda6d853c0f24c816dc72b1501b00993
Erik Smistad <smistad@idi.ntnu.no>
2012-04-14 18:10:55 +0200

    Removed images

--- example.cpp
+++ example.cpp
@@ -1,21 +1,20 @@
 #include "Core.h"
 using namespace SIPL;
 
-int main(int argc, char ** argv)
-{
+int main(int argc, char ** argv) {
     Init();
 
 	Image<PIXEL_COLOR_UCHAR> image = Image<PIXEL_COLOR_UCHAR>("images/example.bmp");
     image.show();
     for(int i = 0; i < image.getWidth(); i++) {
-    for(int j = 0; j < image.getHeight(); j++) {
-        PIXEL_COLOR_UCHAR c;
-        c.green = 255;
-        image.set(i,j,c);
-    }}
+        for(int j = 0; j < image.getHeight(); j++) {
+            PIXEL_COLOR_UCHAR c = image.get(i,j);;
+            if(c.green == 0) 
+                c.green = 100;
+            image.set(i,j,c);
+        }
+    }
     image.update();
-	Image<PIXEL_COLOR_UCHAR> image2 = Image<PIXEL_COLOR_UCHAR>("images/example.bmp");
-    image2.show();
 
 	return 0;
 }
