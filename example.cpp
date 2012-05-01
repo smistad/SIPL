a3f9385f7a58334b746837c620605a618f10a6ff
Erik Smistad <smistad@idi.ntnu.no>
2012-05-01 11:19:23 +0200

    updated the example

--- example.cpp
+++ example.cpp
@@ -23,7 +23,7 @@ int main(int argc, char ** argv) {
     Image<float> * im2 = new Image<float>(im);
     im2->show();
 
-    // View the image using a custom level(0->5) and window(0->25)
+    // View the image using a custom level(0.5) and window(0.25)
     im2->show(0.5, 0.25);
 
     // Calculate the gradient of the image and display the vector field using colors
@@ -41,6 +41,27 @@ int main(int argc, char ** argv) {
 
     // Load volume and display one slice on the screen
     // (Use arrow keys up and down to change the slice on screen)
-    Volume<short> * v = new Volume<short>("Case00.mhd");
+    Volume<uchar> * v = new Volume<uchar>("skull.raw", 256, 256, 256);
     v->show();
+    v->show(100, X);
+    v->show(100, X, 60, 100);
+    v->show(60, 100);
+
+    // Convert volume to another data type
+    Volume<float> * v2 = new Volume<float>(v);
+
+    // Calculate 3D gradient of the volume
+    Volume<float3> * vGradient = new Volume<float3>(v->getWidth(), v->getHeight(), v->getDepth());
+    for(int x = 1; x < v->getWidth()-1; x++) {
+        for(int y = 1; y < v->getHeight()-1; y++) {
+            for(int z = 1; z < v->getDepth()-1; z++) {
+                float3 vector;
+                vector.x = 0.5*(v2->get(x+1,y,z)-v2->get(x-1,y,z));
+                vector.y = 0.5*(v2->get(x,y+1,z)-v2->get(x,y-1,z));
+                vector.z = 0.5*(v2->get(x,y,z+1)-v2->get(x,y,z-1));
+                vGradient->set(x,y,z,vector);
+            }
+        }
+    }
+    vGradient->show();
 }
