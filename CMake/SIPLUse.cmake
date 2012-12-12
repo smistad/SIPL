6600863cd7f9af382342a14551b824e8eea03420
Janne Beate Lervik Bakeng <janne.beate.lervik.bakeng@sintef.no>
2012-12-12 14:52:34 +0100

    Rewrote CMakeList.txt to enable use of find_package.

---
+++ CMake/SIPLUse.cmake
@@ -0,0 +1,14 @@
+#------------------------------------------------------------------------------
+# External libraries
+#------------------------------------------------------------------------------
+
+# GTK
+find_package (PkgConfig REQUIRED)
+pkg_check_modules (GTK2 REQUIRED gtk+-2.0 gthread-2.0)
+
+#------------------------------------------------------------------------------
+# Where to look for includes and libraries
+#------------------------------------------------------------------------------
+include_directories( ${SIPL_INCLUDE_DIRS} ${GTK2_INCLUDE_DIRS})
+link_directories (${SIPL_LIBRARY_DIRS} ${GTK2_LIBRARY_DIRS})
+
